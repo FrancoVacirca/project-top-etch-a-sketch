463f7235b679d7f4525b03e2fc48643119aa97f2
Franco <131470535+FrancoVacirca@users.noreply.github.com>
2023-10-12 19:07:29 -0300

    gridSizeRange update

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.getElementById("grid-container");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div'); //creates a new div when called
-        cell.classList.add('grid-cell'); //add the cell a style for css
-        gridContainer.appendChild(cell); //append cell to gridContainer
-    } 
+function createGrid(value) {
+  gridContainer.innerHTML = "";
+
+  const numRows = value;
+  const numCols = value;
+
+  gridContainer.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      const cell = document.createElement("div"); //creates a new div when called
+      cell.classList.add("grid-cell"); //add the cell a style for css
+      gridContainer.appendChild(cell); //append cell to gridContainer
+    }
+  }
+
+  const cellSize = numCols;
+  const cells = document.querySelectorAll(".grid-cell");
+  cells.forEach((cell) => {
+    cell.style.width = cellSize;
+    cell.style.height = cellSize;
+  });
 }
+
+const gridSizeRange = document.getElementById("gridSizeRange");
+const gridSizeLabel = document.getElementById("gridSizeLabel");
+
+function updateGridSizeLabel() {
+  const value = gridSizeRange.value;
+  gridSizeLabel.textContent = value + "x" + value;
+  createGrid(value); //recreate the grid with the new size
+}
+
+gridSizeRange.addEventListener("input", updateGridSizeLabel);
+
+//initialize the grid with the default size
+createGrid(gridSizeRange.value);
+updateGridSizeLabel();
